65613f6f3c042d37bae43df11655d6c04600388f
Aleksander Evensen <ahse03@gmail.com>
2022-04-06 01:02:26 +0200

    getting schemas

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,40 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use sqlx::{mysql::MySqlPoolOptions, MySql, Pool, Row};
 
-fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+struct Connection {
+    pool: Pool<MySql>,
+}
+
+#[tokio::main]
+async fn main() {
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect("mysql://root@localhost:3306")
+        .await
+        .unwrap();
+
+    tauri::Builder::default()
+        .manage(Connection { pool })
+        .invoke_handler(tauri::generate_handler![get_schemas])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[derive(sqlx::FromRow)]
+struct SchemaQuery {
+    Database: String,
+}
+
+#[tauri::command]
+async fn get_schemas(state: tauri::State<'_, Connection>) -> Result<Vec<String>, String> {
+    let schemas = sqlx::query_as::<_, SchemaQuery>("SHOW SCHEMAS")
+        .fetch_all(&state.pool)
+        .await
+        .map_err(|e| e.to_string())?;
+
+    let result: Vec<String> = schemas
+        .iter()
+        .map(|schema| schema.Database.clone())
+        .collect();
+
+    Ok(result)
 }
